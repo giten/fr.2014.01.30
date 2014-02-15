586b64948401727f8e6aeb118b15678d81f709da
giten <vitaliy_sys@tut.by>
2014-02-16 01:28:11 +0300

    Message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Fr20140130::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+#  get "static_pages/home"
+#  get "static_pages/help"
+#  get "static_pages/about"
+#  get "static_pages/contact"
+  
+
+  root              to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help', via: [:get, :post]
+  match '/about',   to: 'static_pages#about', via: [:get, :post]
+  match '/contact', to: 'static_pages#contact', via: [:get, :post]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
